e0444d92f5f94db81e10d7f6b0b991f6a314e189
Shaojie Jiang <shaojie.jiang1@gmail.com>
2025-04-12 13:38:21 +0200

    Add tests for module

--- agentensor/module.py
+++ agentensor/module.py
@@ -18,8 +18,9 @@ class AgentModule(BaseNode[StateT, DepsT, NodeRunEndT]):
     @classmethod
     def get_params(cls) -> list[TextTensor]:
         """Get the parameters of the module."""
-        return [
-            attr
-            for attr in cls.__dict__.values()
-            if isinstance(attr, TextTensor) and attr.requires_grad
-        ]
+        params = []
+        for base in cls.__mro__:
+            for attr_name, attr in base.__dict__.items():
+                if isinstance(attr, TextTensor) and attr.requires_grad:
+                    params.append(attr)
+        return params
